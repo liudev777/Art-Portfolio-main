820fb0ac2cda95c1f851a83abf1e6dc281ae53cf
Devin <liudev777@gmail.com>
2022-12-26 20:32:26 -0600

    small change

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ import RootLayout from './layouts/RootLayout';
  
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path ="/Art-Portfolio-main/" element={<RootLayout />}>
+    <Route path ="/Art-Portfolio-main" element={<RootLayout />}>
       <Route index element={<Home />} /> 
       <Route path="home" element={<Home />} />
       <Route path="about" element={<About />}/>
