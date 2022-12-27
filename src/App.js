138c68d279049c6725fbb8a9c64fe8c70754721f
Devin <liudev777@gmail.com>
2022-12-27 11:01:27 -0600

    removed additional home route

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path ="/Art-Portfolio-main" element={<RootLayout />}>
       <Route index element={<Home />} /> 
-      <Route path="home" element={<Home />} />
+      {/* <Route path="home" element={<Home />} /> */}
       <Route path="about" element={<About />}/>
       <Route path="contact" element={<Contact />} />
     </Route>
