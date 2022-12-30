9623c2ee65e72db1ad81153016a87be6fbe6c082
Devin <liudev777@gmail.com>
2022-12-29 21:06:52 -0600

    export image stuff

---
+++ src/assets/index.js
@@ -0,0 +1,24 @@
+const img1 = {src: ('../../assets/comm.png'), 
+            description: "blah blah blah"};
+const img2 = {src: ('../../assets/gwen copy.png'), 
+            description: "hello"};
+// const img3 = ('../../assets/qi design.png');
+// const img4 = ('../../assets/reol.png');
+// const img5 = ('../../assets/komi 31.png');
+// const img6 = ('../../assets/3d.png');
+// const img7 = ('../../assets/comm7.png');
+// const img8 = ('../../assets/komi 11.png');
+// const img9 = ('../../assets/landscape7.png');
+// const img10 = ('../../assets/landscape15.png');
+// const img11 = ('../../assets/princess.png');
+// const img12 = ('../../assets/pyro oc 2.png');
+// const img13 = ('../../assets/shinobu.png');
+// const img14 = ('../../assets');
+// const img15 = ('../../assets');
+
+
+const imgArr  = [
+    img1,
+    img2
+];
+export default imgArr;
--- src/components/gallery/Gallery.jsx
+++ src/components/gallery/Gallery.jsx
@@ -1,21 +1,29 @@
 import "./gallery.scss";
-
+import imgArr from "../../assets/index.js"
 //image
-const placeholder = require('../../assets/placeholder.jpg');
-const img1 = require('../../assets/comm.png');
-const img2 = require('../../assets/gwen copy.png');
-const img3 = require('../../assets/qi design.png');
-const img4 = require('../../assets/reol.png');
-const img5 = require('../../assets/komi 31.png');
+// const imgArr = []
+
+// imgArr.push(require('../../assets/comm.png'));
+// imgArr.push(require('../../assets/gwen copy.png'));
+// imgArr.push(require('../../assets/qi design.png'));
+// imgArr.push(require('../../assets/reol.png'));
+// imgArr.push(require('../../assets/komi 31.png'));
+// imgArr.push(require('../../assets/3d.png'));
+// imgArr.push(require('../../assets/comm7.png'));
+// imgArr.push(require('../../assets/komi 11.png'));
+// imgArr.push(require('../../assets/landscape7.png'));
+// imgArr.push(require('../../assets/landscape15.png'));
+// imgArr.push(require('../../assets/princess.png'));
+// imgArr.push(require('../../assets/pyro oc 2.png'));
+// imgArr.push(require('../../assets/shinobu.png'));
+// imgArr.push(require('../../assets'));
+// imgArr.push(require('../../assets'));
 
 export default function Gallery() {
   return (
     <div className="gallery">
-      <img src={img1} alt="placeholder"/>
-      <img src={img2} alt="placeholder"/>
-      <img src={img3} alt="placeholder"/>
-      <img src={img4} alt="placeholder"/>
-      <img src={img5} alt="placeholder"/>
+      {imgArr.map((src) => <img key={src.src} src={src.src}/>)}
+      
     </div>
   )
 }
