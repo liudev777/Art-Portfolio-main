d4d1ca140b1885d01a74449ddd937ac5facff9c8
Devin <liudev777@gmail.com>
2022-12-29 18:48:05 -0600

    added new art

--- src/components/gallery/Gallery.jsx
+++ src/components/gallery/Gallery.jsx
@@ -2,15 +2,20 @@ import "./gallery.scss";
 
 //image
 const placeholder = require('../../assets/placeholder.jpg');
+const img1 = require('../../assets/comm.png');
+const img2 = require('../../assets/gwen copy.png');
+const img3 = require('../../assets/qi design.png');
+const img4 = require('../../assets/reol.png');
+const img5 = require('../../assets/komi 31.png');
 
 export default function Gallery() {
   return (
     <div className="gallery">
-      <img src={placeholder} alt="placeholder"/>
-      <img src={placeholder} alt="placeholder"/>
-      <img src={placeholder} alt="placeholder"/>
-      <img src={placeholder} alt="placeholder"/>
-      <img src={placeholder} alt="placeholder"/>
+      <img src={img1} alt="placeholder"/>
+      <img src={img2} alt="placeholder"/>
+      <img src={img3} alt="placeholder"/>
+      <img src={img4} alt="placeholder"/>
+      <img src={img5} alt="placeholder"/>
     </div>
   )
 }
