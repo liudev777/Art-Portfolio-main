96cf459b718d319577eeca79235ff91fe6f42e2b
Devin <liudev777@gmail.com>
2022-12-29 21:31:13 -0600

    added more images

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,24 +1,69 @@
-const img1 = {src: ('../../assets/comm.png'), 
+const img1 = {src: require('./comm.png'), 
             description: "blah blah blah"};
-const img2 = {src: ('../../assets/gwen copy.png'), 
+const img2 = {src: require('./gwen copy.png'), 
             description: "hello"};
-// const img3 = ('../../assets/qi design.png');
-// const img4 = ('../../assets/reol.png');
-// const img5 = ('../../assets/komi 31.png');
-// const img6 = ('../../assets/3d.png');
-// const img7 = ('../../assets/comm7.png');
-// const img8 = ('../../assets/komi 11.png');
-// const img9 = ('../../assets/landscape7.png');
-// const img10 = ('../../assets/landscape15.png');
-// const img11 = ('../../assets/princess.png');
-// const img12 = ('../../assets/pyro oc 2.png');
-// const img13 = ('../../assets/shinobu.png');
+const img3 = {src: require('./qi design.png'), 
+            decription: "lol"};
+const img4 = {src: require('./reol.png'), 
+            description: "N/A"};
+const img5 = {src: require('./komi 31.png'), 
+            description: "N/A"};
+const img6 = {src: require('./3d.png'), 
+            description: "N/A"};
+const img7 = {src: require('./comm7.png'), 
+            description: "N/A"};
+const img8 = {src: require('./komi 11.png'), 
+            description: "N/A"};
+const img9 = {src: require('./landscape7.png'), 
+            description: "N/A"};
+const img10 = {src: require('./landscape15.png'), 
+            description: "N/A"};
+const img11 = {src: require('./princess.png'), 
+            description: "N/A"};
+const img12 = {src: require('./pyro oc 2.png'), 
+            description: "N/A"};
+const img14 = {src: require('./studd5.png'), 
+            description: "N/A"};
+const img15 = {src: require('./studdd.png'), 
+            description: "N/A"};
+const img16 = {src: require('./Untitled_Artwork(1).png'), 
+            description: "N/A"};
+const img17 = {src: require('./Untitled_Artwork(2).png'), 
+            description: "N/A"};
+const img18 = {src: require('./Untitled_Artwork(3).png'), 
+            description: "N/A"};
+const img19 = {src: require('./Untitled_Artwork(4).png'), 
+            description: "N/A"};
+const img20 = {src: require('./Untitled_Artwork(5).png'), 
+            description: "N/A"};
+const img21 = {src: require('./Untitled_Artwork(6).png'), 
+            description: "N/A"};
+const img22 = {src: require('./Untitled_Artwork.png'), 
+            description: "N/A"};
+
 // const img14 = ('../../assets');
 // const img15 = ('../../assets');
 
 
-const imgArr  = [
+export const imgArr  = [
     img1,
-    img2
+    img2,
+    img3,
+    img4,
+    img5,
+    img6,
+    img7,
+    img8,
+    img10,
+    img11,
+    img12,
+    img14,
+    img15,
+    img16,
+    img17,
+    img18,
+    img19,
+    img20,
+    img21,
+    img22
 ];
-export default imgArr;
--- src/components/gallery/Gallery.jsx
+++ src/components/gallery/Gallery.jsx
@@ -1,28 +1,11 @@
 import "./gallery.scss";
-import imgArr from "../../assets/index.js"
-//image
-// const imgArr = []
-
-// imgArr.push(require('../../assets/comm.png'));
-// imgArr.push(require('../../assets/gwen copy.png'));
-// imgArr.push(require('../../assets/qi design.png'));
-// imgArr.push(require('../../assets/reol.png'));
-// imgArr.push(require('../../assets/komi 31.png'));
-// imgArr.push(require('../../assets/3d.png'));
-// imgArr.push(require('../../assets/comm7.png'));
-// imgArr.push(require('../../assets/komi 11.png'));
-// imgArr.push(require('../../assets/landscape7.png'));
-// imgArr.push(require('../../assets/landscape15.png'));
-// imgArr.push(require('../../assets/princess.png'));
-// imgArr.push(require('../../assets/pyro oc 2.png'));
-// imgArr.push(require('../../assets/shinobu.png'));
-// imgArr.push(require('../../assets'));
-// imgArr.push(require('../../assets'));
+import {imgArr} from "../../assets/index.js"
+// console.log(imgArr);
 
 export default function Gallery() {
   return (
     <div className="gallery">
-      {imgArr.map((src) => <img key={src.src} src={src.src}/>)}
+      {imgArr.map(({src, description}) => <img key={src} src={src}/>)}
       
     </div>
   )
