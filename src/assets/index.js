d25c87791fc65c68d0ee2e19c727a343b978b4e2
Devin <liudev777@gmail.com>
2022-12-31 16:38:17 -0600

    created profile view

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,64 +1,64 @@
-const img1 = {src: require('./comm.png'), 
+const img1 = {src: require('./comm.jpg'), 
             description: "Commission art for Vtuber Junipee",
             title: "Junipee"};
-const img2 = {src: require('./gwen copy.png'), 
+const img2 = {src: require('./gwen copy.jpg'), 
             description: "Gwen Stacy from the Spider-Man Into the Spider-verse movie",
             title: "Gwen Stacy"};
-const img3 = {src: require('./qi design.png'), 
+const img3 = {src: require('./qi design.jpg'), 
             decription: "Character design sheet for my original character Qi",
             title: "Qi Design"};
-const img4 = {src: require('./reol.png'), 
+const img4 = {src: require('./reol.jpg'), 
             description: "Fanart for Japanese pop artist Reol", 
             title: "Reol"};
-const img5 = {src: require('./komi 31.png'), 
+const img5 = {src: require('./komi 31.jpg'), 
             description: "Day 31 Komi fanart from my '31 Day Komi Challenge'", 
             title: "Komi-san in Kimono"};
-const img6 = {src: require('./3d.png'), 
+const img6 = {src: require('./3d.jpg'), 
             description: "A terminator line and shape study", 
             title: "Terminator line study"};
-const img7 = {src: require('./comm7.png'), 
+const img7 = {src: require('./comm7.jpg'), 
             description: "Commission art for uhhhh", 
             title: "Commission Art"};
-const img8 = {src: require('./komi 11.png'), 
+const img8 = {src: require('./komi 11.jpg'), 
             description: "Komi-san pokemon parody art; part of my '31 Day Komi Challenge' collection", 
             title: "Komimon"};
-const img9 = {src: require('./landscape7.png'), 
+const img9 = {src: require('./landscape7.jpg'), 
             description: "Swamp landscape art", 
             title: "Swamp"};
-const img10 = {src: require('./landscape15.png'), 
+const img10 = {src: require('./landscape15.jpg'), 
             description: "Eerie landscape art", 
             title: "Eerie"};
-const img11 = {src: require('./princess.png'), 
+const img11 = {src: require('./princess.jpg'), 
             description: "Study of a princess ref found on pinterest", 
             title: "Pearl Princess"};
-const img12 = {src: require('./pyro oc 2.png'), 
+const img12 = {src: require('./pyro oc 2.jpg'), 
             description: "Fanart of Asuka Soryu Langley from Neon Genesis Evangelion", 
             title: "A lonely melody"};
-const img14 = {src: require('./studd5.png'), 
+const img14 = {src: require('./studd5.jpg'), 
             description: "Realism study of a cosplayer", 
             title: "Purple"};
-const img15 = {src: require('./studdd.png'), 
+const img15 = {src: require('./studdd.jpg'), 
             description: "Realism fabric study of girl by lake", 
             title: "Blue"};
-const img16 = {src: require('./Untitled_Artwork(1).png'), 
+const img16 = {src: require('./Untitled_Artwork(1).jpg'), 
             description: "Original Character art of Qi", 
             title: "Qi"};
-const img17 = {src: require('./Untitled_Artwork(2).png'), 
+const img17 = {src: require('./Untitled_Artwork(2).jpg'), 
             description: "I got bored and decided to do my Calculus III homework and draw at the same time. (I ended up with a B in the class)", 
             title: "Calculus III"};
-const img18 = {src: require('./Untitled_Artwork(3).png'), 
+const img18 = {src: require('./Untitled_Artwork(3).jpg'), 
             description: "Study compliation during the month of September", 
             title: "Study Compilation dssfsdfsfsdf"};
-const img19 = {src: require('./Untitled_Artwork(4).png'), 
+const img19 = {src: require('./Untitled_Artwork(4).jpg'), 
             description: "Study of a girl walking down the street", 
             title: "时间如水"};
-const img20 = {src: require('./Untitled_Artwork(5).png'), 
+const img20 = {src: require('./Untitled_Artwork(5).jpg'), 
             description: "Character study of person under umbrella", 
             title: "Snow"};
-const img21 = {src: require('./Untitled_Artwork(6).png'), 
+const img21 = {src: require('./Untitled_Artwork(6).jpg'), 
             description: "Just wanted to flex on Pyro", 
             title: "Portrait Study"};
-const img22 = {src: require('./Untitled_Artwork.png'), 
+const img22 = {src: require('./Untitled_Artwork.jpg'), 
             description: "Study of battle maid cosplay", 
             title: "Not exactly 2B"};
 
--- src/components/cover/Cover.jsx
+++ src/components/cover/Cover.jsx
@@ -2,11 +2,19 @@ import "./cover.scss"
 
 //img
 const placeholder = require("../../assets/placeholder.jpg");
-const landscape = require("../../assets/landscape.png");
+const landscape = require("../../assets/landscape.jpg");
 
 export default function Cover() {
   return (
     <div className="cover" id="cover">
+      <div className="profile">
+        <div className="pfp"></div>
+        <div className="name">Devin Liu</div>
+        <div className="status">Student, Illustrator, Programmer</div>
+        <div className="location">Chicago, IL</div>
+        <div className="social"></div>
+      </div>
+
       <img src={landscape} alt="placeholder" />
     </div>
   )
